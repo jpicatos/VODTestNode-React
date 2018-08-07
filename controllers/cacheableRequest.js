ba024746f4df002d62bd7b768df9b575fae102b9
jpicatos <32961669+jpicatos@users.noreply.github.com>
2018-08-07 13:04:33 +0200

    Add files via upload

---
+++ controllers/cacheableRequest.js
@@ -0,0 +1,33 @@
+var request = require('request');
+var cacheProvider = require('./../cacheProvider');
+
+const CACHE_DURATION = 720; // 12 minutes
+const CACHE_KEY = 'DATA';
+
+
+module.exports = function(cb){
+    var options = {
+        url: 'https://sela-test.herokuapp.com/assets/hkzxv.json',
+        headers: {
+            'content-type': 'application/json'
+        }
+    };
+    cacheProvider.instance().get(CACHE_KEY, function(err, value){
+        if(err) console.error(error);
+        if(value === undefined){
+            request(options, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    jsonResData = JSON.parse(body);
+                    cacheProvider.instance().set(CACHE_KEY, jsonResData, CACHE_DURATION, function(err, success){
+                        if(!err && success){
+                            cb(jsonResData);
+                        }
+                    });
+                }
+            });
+        }
+        else{
+            cb(value);
+        }
+    });
+}
--- public/assets/js/history.js
+++ public/assets/js/history.js
@@ -1,92 +1,30 @@
 $(document).ready(function(){
-	$.getJSON('https://cors-anywhere.herokuapp.com/https://sela-test.herokuapp.com/assets/hkzxv.json', function(generalData){
-		$.getJSON('last_videos.php',function(historyData){
-			console.log(historyData);
-			console.log(generalData);
-			if (historyData.items!=null) {
-				$('.items em').hide(800);
-				completeLastSeenVideos(generalData,historyData.items.length,historyData.items);	
-			}
-					
-		});
-	});
-
-	$('.focused a').focus();
-
+	$('#deleteHistory').on('click', function(){
+        $.ajax({
+            type: 'DELETE',
+            url: '/history',
+            success: function(data){
+            	location.reload();
+            }
+        });
+    });
 	$('select[name=number]').change(function(event) {
 		var formData = {
 			'number'              : $('select[name=number]').val(),
 		};
 		$.ajax({
 			type        : 'POST',
-			url         : 'last_videos.php',
+			url         : '/history',
 			data        : formData,
-			dataType    : 'json',
-			encode          : true
-		})
-	
-			.done(function(data) {
-				$.getJSON('https://cors-anywhere.herokuapp.com/https://sela-test.herokuapp.com/assets/hkzxv.json', function(generalData){
-						console.log(data);
-						console.log(generalData);
-						if (data.items!=null) {
-							$('#last_videos').html('');
-							completeLastSeenVideos(generalData,data.number,data.items);
-						}
-				});
-			});
-	
-		event.preventDefault();
+			encode          : true,
+			success: function(data) {
+				/*location.reload();*/
+			}
+		});
 	});
-});
-function searchVideo(data, idVideo) {
-	var i = 0
-	var founded = false;
-	while (i < data.totalCount && !founded) {
-		var videoId = data.entries[i].id;
-		var video = data.entries[i].contents[0].url;
-		if (videoId == idVideo) {
-			founded = true;
-		}
-		else{
-			i++;
-		}
-	}
-	return i;
-}
-function completeLastSeenVideos(data, number, content){
-	var numVideos = number;
-	var start = 0;
-	if(number > content.length || number == -1){
-		numVideos = content.length;
-	}
-	else{
-		start = content.length-numVideos;
-	}
-	console.log(number);
-	for (var i = start; i < content.length; i++) {
-		var index = searchVideo(data, content[i].id);
-		var preHtml = $('#last_videos').html();
-		var item = data.entries[index];
-		var videoLink = '../video/video.php?id='+item.id;
-		var imageLink = item.images[0].url;
-		var title = item.title;
-		title = fetchTitle(title);
-		var category = item.categories[0].title;
-		var type = item.type;
-		type = fetchType(type);
-		var languaje = item.metadata[0].value;
-		languaje = fetchLanguaje(languaje);
 
-		var image = '<div class="thumbnail"><img onerror=this.src="../../imagen.jpg" src="'+imageLink+'"></div>';
-		var text = '<h2>'+title+'</h2><h3>'+category+' | '+type+' | '+languaje+'</h3>';
-		var link = '<a href="'+videoLink+'"><div>'+ image + text +'</div></a>';
-		var htmlItem = '<li class="item focusable">'+link+'</li>';
-		$('#last_videos').hide();
-		$('#last_videos').html(htmlItem + preHtml);
-		$('#last_videos').fadeIn('fast');
-	}
-}
+	$('.focused a').focus();
+});
 
 $(document).keydown(function(e){
 	$('#numberSelect').hide();
